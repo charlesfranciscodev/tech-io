6bb7c2632f358dcccfeab51bf6ec379384082f1d
Charles-Antoine Francisco <charlantfr@gmail.com>
2018-02-02 11:00:14 -0500

    udpate dijkstra.py

--- dijkstra.py
+++ dijkstra.py
@@ -1,11 +1,13 @@
-def set_current_node(visited, currentDistances):
-    # visited is a set that contains the names of the nodes marked as visited.
-    # currentDistances is a dictionary that contains the current minimum distance of each node.
+def set_current_node(visited, distances):
+    """Returns the node that should be set as current node.
+    visited -- set that contains the nodes marked as visited
+    distances -- dictionary mapping each node to its current distance
+    """
     min_distance = 100
     current_node = ""
-    for (node, distance) in currentDistances.items():
-      if (node not in visited and distance < min_distance):
-        min_distance = distance
-        current_node = node
+    for (node, distance) in distances.items():
+        if (node not in visited and distance < min_distance):
+            min_distance = distance
+            current_node = node
 
     return current_node
